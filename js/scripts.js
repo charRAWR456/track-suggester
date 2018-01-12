943ff0565052a4b74be8912f03f8136f24c7d224
Jamie Pittak <charrawr456@gmail.com>
2018-01-12 12:55:01 -0800

    adds radio buttons instead of dropdown for first question, functional

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,25 @@
-(document).ready(function(){
+$(document).ready(function(){
 
-  $("form#purchase").submit(function(event) {
-    event.preventDefault();
+  $("#submit-button").click(function() {
     var cSharp = 0;
     var java = 0;
     var css = 0;
     var php = 0;
     var ruby = 0;
-    var type = $("#type").val();
+    var name =$("#name").val();
 
-    if ($(type) === "cSharp"){
-      alert("yes");
-    }
-    else ($(type) != "cSharp")
-    {alert("no");
-  };
+  if ($("input:radio[name=type]:checked").val() == "cSharp") {
+    cSharp += 1;
+  } else if($("input:radio[name=type]:checked").val() == "java") {
+    java += 1;
+  } else if($("input:radio[name=type]:checked").val() == "css") {
+    css += 1;
+  }  else if ($("input:radio[name=type]:checked").val() == "php") {
+    php += 1;
+  } else if ($("input:radio[name=type]:checked").val() == "ruby") {
+    ruby += 1;
+  }
+
   if ($("input:radio[name=language]:checked").val() == "cSharp") {
     cSharp += 1;
   } else if($("input:radio[name=language]:checked").val() == "java") {
@@ -23,8 +28,7 @@
     css += 1;
   }  else if ($("input:radio[name=language]:checked").val() == "php") {
     php += 1;
-  }
-  else ($("input:radio[name=language]:checked").val() == "ruby") {
+  } else if ($("input:radio[name=language]:checked").val() == "ruby") {
     ruby += 1;
   }
 
@@ -36,8 +40,7 @@
     css += 1;
   } else if ($("input:radio[name=language]:checked").val() == "php") {
     php += 1;
-  }
-  else ($("input:radio[name=language]:checked").val() == "ruby") {
+  } else if ($("input:radio[name=language]:checked").val() == "ruby") {
     ruby += 1;
   }
 
@@ -49,8 +52,7 @@
     css += 1;
   } else if ($("input:radio[name=task]:checked").val() == "php") {
     php += 1;
-  }
-  else ($("input:radio[name=task]:checked").val() == "ruby") {
+  } else if ($("input:radio[name=task]:checked").val() == "ruby") {
     ruby += 1;
   }
 
@@ -62,8 +64,7 @@
     css += 1;
   } else if ($("input:radio[name=environment]:checked").val() == "php") {
     php += 1;
-  }
-  else ($("input:radio[name=environment]:checked").val() == "ruby") {
+  } else if ($("input:radio[name=environment]:checked").val() == "ruby") {
     ruby += 1;
   }
 
@@ -75,11 +76,10 @@
     css += 1;
   } else if ($("input:radio[name=workPlace]:checked").val() == "php") {
     php += 1;
-  }
-  else ($("input:radio[name=workPlace]:checked").val() == "ruby") {
+  } else if ($("input:radio[name=workPlace]:checked").val() == "ruby") {
     ruby += 1;
   }
 
-  alert("cSharp = " + cSharp + " java = " + java + " CSS = " + css + "PHP = " + php + " ruby = " + ruby)
-
+  alert("Hi "+ name+"! based off your answers you should take"+ " cSharp = " + cSharp + " java = " + java + " CSS = " + css + "PHP = " + php + " ruby = " + ruby)
+});
 });
